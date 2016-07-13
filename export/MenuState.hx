a4b97b40150a4fa55a99e021bfba055be43e4b31
MShnik <Mgpshnik@gmail.com>
2016-07-12 21:50:49 -0400

    Initial commit

---
+++ export/MenuState.hx
@@ -0,0 +1,22 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.text.FlxText;
+import flixel.ui.FlxButton;
+import flixel.math.FlxMath;
+
+class MenuState extends FlxState
+{
+	override public function create():Void
+	{
+		trace("Created!");
+		super.create();
+	}
+
+	override public function update(elapsed:Float):Void
+	{
+		super.update(elapsed);
+	}
+}
