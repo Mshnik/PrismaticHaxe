d99b0ed4e9784e06176898205c2c28311ccf948c
MShnik <Mgpshnik@gmail.com>
2016-07-21 13:45:52 -0400

    Added preinitialized fields to Point, added neighbor calculation, added tests for those

--- source/common/Point.hx
+++ source/common/Point.hx
@@ -6,7 +6,17 @@ class Point {
   private static var currentPoolSize = 0;
   private static var MAX_POOL_SIZE = 256;
 
-  public static function get(row : Int, col : Int) : Point {
+  public static var ZERO : Point = get(0,0);
+  public static var UP : Point = get(-1,0);
+  public static var DOWN : Point = get(1,0);
+  public static var LEFT : Point = get(0,-1);
+  public static var RIGHT : Point = get(0,1);
+  public static var UPLEFT : Point = get(-1,-1);
+  public static var UPRIGHT : Point = get(-1,1);
+  public static var DOWNLEFT : Point = get(1,-1);
+  public static var DOWNRIGHT : Point = get(1,1);
+
+  public static inline function get(row : Int, col : Int) : Point {
     var s = createString(row, col);
     if (allocatedPoints.exists(s)) {
       return allocatedPoints.get(s);
@@ -14,8 +24,18 @@ class Point {
     return new Point(row, col);
   }
 
-  public static function clearPool() : Void {
+  public static inline function clearPool() : Void {
     allocatedPoints = new StringMap<Point>();
+
+    allocatedPoints.set(createStringOf(ZERO), ZERO);
+    allocatedPoints.set(createStringOf(UP), UP);
+    allocatedPoints.set(createStringOf(DOWN), DOWN);
+    allocatedPoints.set(createStringOf(LEFT), LEFT);
+    allocatedPoints.set(createStringOf(RIGHT), RIGHT);
+    allocatedPoints.set(createStringOf(UPLEFT), UPLEFT);
+    allocatedPoints.set(createStringOf(UPRIGHT), UPRIGHT);
+    allocatedPoints.set(createStringOf(DOWNRIGHT), DOWNRIGHT);
+    allocatedPoints.set(createStringOf(DOWNLEFT), DOWNLEFT);
   }
 
   public var row(default, null) : Int;
@@ -30,16 +50,20 @@ class Point {
     }
   }
 
-  public function add(p : Point) : Point {
+  public inline function add(p : Point) : Point {
     return get(row + p.row, col + p.col);
   }
 
-  public function subtract(p : Point) : Point {
+  public inline function subtract(p : Point) : Point {
     return get(row - p.row, col - p.col);
   }
 
-  public function dot(p : Point) : Int {
+  public inline function dot(p : Point) : Int {
     return row * p.row + col * p.col;
+  }
+
+  private static inline function createStringOf(p : Point) : String {
+    return createString(p.row, p.col);
   }
 
   private static inline function createString(row : Int, col : Int) : String {
@@ -52,6 +76,17 @@ class Point {
 
   public inline function toString() : String {
     return createString(row, col);
+  }
+
+  /** Returns the locations considered neighboring the given point.
+   * May include points that will be OOB in an Array2D, so should be filtered before applied.
+   **/
+  public inline function getNeighbors() : Array<Point> {
+    if (col % 2 == 0) {
+      return [add(UP), add(UPRIGHT), add(RIGHT), add(DOWN), add(LEFT),add(UPLEFT)];
+    } else {
+      return [add(UP), add(RIGHT), add(DOWNRIGHT), add(DOWN), add(DOWNLEFT),add(LEFT)];
+    }
   }
 
 }
--- source/test/TestPoint.hx
+++ source/test/TestPoint.hx
@@ -3,36 +3,40 @@ package test;
 import common.Point;
 class TestPoint extends TestCase {
 
+  private static inline function get(row : Int, col : Int) : Point {
+    return Point.get(row, col);
+  }
+
   private function checkPoint(row : Int, col : Int, actual : Point) {
     assertEquals(row, actual.row);
     assertEquals(col, actual.col);
     assertEquals("(" + row + "," + col + ")", actual.toString());
-    assertEquals(Point.get(row, col), actual);
+    assertEquals(get(row, col), actual);
   }
 
   public function testCreation() {
-    checkPoint(0, 0, Point.get(0, 0));
-    checkPoint(2, 3, Point.get(2, 3));
-    checkPoint(-1, -1, Point.get(-1, -1));
-    checkPoint(10, -15, Point.get(10, -15));
+    checkPoint(0, 0, get(0, 0));
+    checkPoint(2, 3, get(2, 3));
+    checkPoint(-1, -1, get(-1, -1));
+    checkPoint(10, -15, get(10, -15));
   }
 
   public function testEquality() {
-    assertTrue(Point.get(0,0).equals(Point.get(0,0)));
-    assertTrue(Point.get(1,0).equals(Point.get(1,0)));
-    assertTrue(Point.get(0,2).equals(Point.get(0,2)));
-    assertTrue(Point.get(0,0).equals(Point.get(0,0)));
-    assertTrue(Point.get(-1,0).equals(Point.get(-1,0)));
-    assertTrue(Point.get(0,-2).equals(Point.get(0,-2)));
-    assertFalse(Point.get(0,1).equals(Point.get(0,0)));
-    assertFalse(Point.get(1,0).equals(Point.get(0,0)));
-    assertFalse(Point.get(1,1).equals(Point.get(0,0)));
+    assertTrue(get(0,0).equals(get(0,0)));
+    assertTrue(get(1,0).equals(get(1,0)));
+    assertTrue(get(0,2).equals(get(0,2)));
+    assertTrue(get(0,0).equals(get(0,0)));
+    assertTrue(get(-1,0).equals(get(-1,0)));
+    assertTrue(get(0,-2).equals(get(0,-2)));
+    assertFalse(get(0,1).equals(get(0,0)));
+    assertFalse(get(1,0).equals(get(0,0)));
+    assertFalse(get(1,1).equals(get(0,0)));
 
   }
 
   public function testMath() {
-    var p : Point = Point.get(1, 2);
-    var p2 : Point = Point.get(3, 4);
+    var p : Point = get(1, 2);
+    var p2 : Point = get(3, 4);
     checkPoint(4, 6, p.add(p2));
     checkPoint(1, 2, p);
     checkPoint(3, 4, p2);
@@ -46,5 +50,47 @@ class TestPoint extends TestCase {
     checkPoint(3, 4, p2);
   }
 
+  public function testUnits() {
+    assertEquals(Point.ZERO, get(0,0));
+    assertEquals(Point.UP, get(-1,0));
+    assertEquals(Point.DOWN, get(1,0));
+    assertEquals(Point.LEFT, get(0,-1));
+    assertEquals(Point.RIGHT, get(0,1));
+    assertEquals(Point.UPLEFT, get(-1,-1));
+    assertEquals(Point.UPRIGHT, get(-1,1));
+    assertEquals(Point.DOWNLEFT, get(1,-1));
+    assertEquals(Point.DOWNRIGHT, get(1,1));
+
+    var p = Point.ZERO;
+    var p2 = Point.UP;
+    var p3 = Point.DOWN;
+    var p4 = Point.LEFT;
+    var p5 = Point.RIGHT;
+    var p6 = Point.UPLEFT;
+    var p7 = Point.UPRIGHT;
+    var p8 = Point.DOWNLEFT;
+    var p9 = Point.DOWNRIGHT;
+
+    Point.clearPool();
+
+    assertEquals(Point.ZERO, p);
+    assertEquals(Point.UP, p2);
+    assertEquals(Point.DOWN, p3);
+    assertEquals(Point.LEFT, p4);
+    assertEquals(Point.RIGHT, p5);
+    assertEquals(Point.UPLEFT, p6);
+    assertEquals(Point.UPRIGHT, p7);
+    assertEquals(Point.DOWNLEFT, p8);
+    assertEquals(Point.DOWNRIGHT, p9);
+  }
+
+  public function testNeighbors() {
+    var p = get(2,2);
+    assertArrayEquals([get(1,2), get(1,3),get(2,3), get(3,2), get(2,1), get(1,1)], p.getNeighbors());
+
+    var p2 = get(4,5);
+    assertArrayEquals([get(3,5), get(4,6),get(5,6), get(5,5), get(5,4), get(4,4)], p2.getNeighbors());
+
+  }
 
 }
