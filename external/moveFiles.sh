bfeb8bf83a58607ff9fae6e8b4f756ec24f09ee6
MShnik <Mgpshnik@gmail.com>
2016-08-08 09:18:15 -0400

    Added braces around every variable because intelliJ told me to

--- external/moveFiles.sh
+++ external/moveFiles.sh
@@ -2,44 +2,44 @@
 
 curPath=$(pwd)
 
-if [[ $curPath != *"PrismaticHaxe" ]]
+if [[ ${curPath} != *"PrismaticHaxe" ]]
 then
   echo "Current path is wrong, should be run with project root as WD"
-  echo "Current path: "$curPath
+  echo "Current path: "${curPath}
   exit 2
 fi
 
-fromPath=$curPath$"/export/mac64/neko/bin/PrismaticHaxe.app/Contents/Resources/assets/data"
-toPath=$curPath$"/assets/data"
+fromPath=${curPath}$"/export/mac64/neko/bin/PrismaticHaxe.app/Contents/Resources/assets/data"
+toPath=${curPath}$"/assets/data"
 
 echo "Performing file moving"
-echo "From "$fromPath
-echo "To "$toPath
+echo "From "${fromPath}
+echo "To "${toPath}
 
-if test -d $fromPath
+if test -d ${fromPath}
   then
   if [[ $# == 0 ]]
     # Whole folder mode. Copies entire assets/data folder
     then
       echo "Moving all"
-      if test -d $toPath
+      if test -d ${toPath}
         then
-        rm -r $toPath
+        rm -r ${toPath}
       fi
-      cp -rf $fromPath $toPath
+      cp -rf ${fromPath} ${toPath}
   else
     # Specific file mode. Copies specified files within assets/data
     for f in "$@" 
     do
-      fromFile=$fromPath$"/"$f
-      toFile=$toPath$"/"$f
+      fromFile=${fromPath}$"/"${f}
+      toFile=${toPath}$"/"${f}
 
-      if test -e $fromFile
+      if test -e ${fromFile}
         then
-        echo $"Moving "$f
-        cp -f $fromFile $toFile
+        echo $"Moving "${f}
+        cp -f ${fromFile} ${toFile}
       else
-        echo $"Data file "$f$" doesn't exist"
+        echo $"Data file "${f}$" doesn't exist"
       fi
     done
   fi    
