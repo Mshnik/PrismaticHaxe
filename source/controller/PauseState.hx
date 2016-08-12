e8572b22fa852e92390e8b4bed2e77ddf0db0503
MShnik <Mgpshnik@gmail.com>
2016-08-12 19:04:57 -0400

    Moved assets into two subfolders for game vs pause menu. Gave pause state a background, made close button a text X

--- source/controller/PauseState.hx
+++ source/controller/PauseState.hx
@@ -1,19 +1,27 @@
 package controller;
 
-import flixel.util.FlxColor;
+import flixel.FlxSprite;
 import flixel.FlxG;
 import flixel.ui.FlxButton;
 import flixel.FlxSubState;
 
 class PauseState extends FlxSubState {
 
+  private var bg : FlxSprite;
   private var closeBtn : FlxButton;
   private var toLevelSelectBtn : FlxButton;
 
   public override function create() {
     super.create();
 
-    closeBtn = new FlxButton(FlxG.width * 0.5, FlxG.height * 0.5, "Close", onCloseButtonClicked);
+    bg = new FlxSprite().loadGraphic(AssetPaths.pause_bg__png);
+    bg.updateHitbox();
+    bg.x = (FlxG.width-bg.width)/2;
+    bg.y = (FlxG.height-bg.height)/2;
+    add(bg);
+
+    closeBtn = new FlxButton(bg.x + 12, bg.y + 12, "", onCloseButtonClicked);
+    closeBtn.loadGraphic(AssetPaths.pause_close_btn__png); //TODO - make this a three state graphic for normal/hover/press
     add(closeBtn);
 
     toLevelSelectBtn = new FlxButton(FlxG.width * 0.5, FlxG.height * 0.5 + 40, "Level Select", onLevelSelectButtonClicked);
